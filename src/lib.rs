2ee9783c38b9450a134925b2384898449a68242d
Mark Schmale <masch@masch.it>
2014-12-07 06:06:46 +0100

    added some more crypto. close to unlock keys..

--- src/lib.rs
+++ src/lib.rs
@@ -7,26 +7,3 @@ extern crate "rust-crypto" as rust_crypto;
 pub use keychain::Keychain;
 
 mod keychain;
-
-
-#[cfg(test)]
-
-mod unittest {
-
-    use keychain::Keychain;
-
-    #[test]
-    fn load_keychain() {
-        let kc_path = Path::new("./keys.agilekeychain");
-        let kc = Keychain::from_file(&kc_path).unwrap();
-
-        assert!(kc.path == kc_path);
-    }
-
-    #[test]
-    fn unlock_keychain() {
-        let kc_path = Path::new("./keys.agilekeychain");
-        let kc = Keychain::from_file(&kc_path).unwrap();
-        kc.open("asdf");
-    }
-}
